3cf6a79e84e4158ad0cb1d95accb063ee6c80979
unknown <syedabbas83778@gmail.com>
2023-11-19 20:15:02 -0800

    src deleted

--- src/constants/file_constants.ts
+++
@@ -1,6 +0,0 @@
-export const DIRECTORIES = {
-    COMPANY_LOGO: 'company_logos',
-    USER_PROFILES: 'user_profiles',
-    LMS_VIDEOS:'lms_videos',
-  }
-  
--- src/models/lib/app_error.ts
+++
@@ -1,7 +0,0 @@
-export default class AppError extends Error {
-    additionalInfo?:string
-    constructor (message,additionalInfo?){
-        super(message)
-        this.additionalInfo=additionalInfo
-    }
-}
